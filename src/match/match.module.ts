55893d327f54937649b8984bf0648f650c6e81ad
Sophia <beyondthesophia@gmail.com>
2021-10-11 03:32:25 +0900

    fix(match): 매치 모듈에서 ORM 모델을 로드하도록 수정

--- src/match/match.module.ts
+++ src/match/match.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { MatchService } from "@match/match.service";
 import { MatchResolver } from "@match/match.resolver";
 
+import Match from "@match/models/match.model";
+
 @Module({
+    imports: [TypeOrmModule.forFeature([Match])],
     providers: [MatchService, MatchResolver],
     exports: [MatchService],
 })
