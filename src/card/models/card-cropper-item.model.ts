ccc22716fffb8b18df822f06846b6b4fa8119d4d
Sophia <beyondthesophia@gmail.com>
2021-11-15 17:56:34 +0900

    fix(card): 덱 대표 카드 데이터 모델 누락된 항목 추가

---
+++ src/card/models/card-cropper-item.model.ts
@@ -0,0 +1,46 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, RelationId, UpdateDateColumn } from "typeorm";
+import { Field, Int, ObjectType } from "@nestjs/graphql";
+
+import { Card } from "@card/models/Card.model";
+
+@Entity({ name: "cropper-items" })
+@ObjectType()
+export class CardCropperItem {
+    @Field(() => Int)
+    @PrimaryGeneratedColumn({ type: "int" })
+    public id!: number;
+
+    @Field(() => Int)
+    @Column({ type: "int" })
+    public x!: number;
+
+    @Field(() => Int)
+    @Column({ type: "int" })
+    public y!: number;
+
+    @Field(() => Int)
+    @Column({ type: "int" })
+    public width!: number;
+
+    @Field(() => Int)
+    @Column({ type: "int" })
+    public height!: number;
+
+    @Field(() => Date)
+    @CreateDateColumn()
+    public createdAt: Date;
+
+    @Field(() => Date)
+    @UpdateDateColumn()
+    public updatedAt: Date;
+
+    //
+    // Relation (One-to-One) - Card => CropperItem
+    //
+    @OneToOne(() => Card)
+    @JoinColumn()
+    public card!: Card;
+
+    @RelationId((entity: CardCropperItem) => entity.card)
+    public cardId!: Card["id"];
+}
