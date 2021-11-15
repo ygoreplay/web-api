5be3be9f82396543ce64695be36468163ca933cb
Sophia <beyondthesophia@gmail.com>
2021-11-15 17:26:01 +0900

    refactor(card): 카드 데이터베이스 업데이트 로직 수정

--- src/utils/cards.ts
+++ src/utils/cards.ts
@@ -56,17 +56,32 @@ export function replaceCard(currentCard: Card, newCard: YGOProCard) {
 }
 
 export function replaceText(currentText: Text, newText: Text) {
-    Object.keys(currentText).forEach(key => {
-        currentText[key] = newText[key];
-    });
+    currentText.name = newText.name;
+    currentText.desc = newText.desc;
+    currentText.str1 = newText.str1;
+    currentText.str2 = newText.str2;
+    currentText.str3 = newText.str3;
+    currentText.str4 = newText.str4;
+    currentText.str5 = newText.str5;
+    currentText.str6 = newText.str6;
+    currentText.str7 = newText.str7;
+    currentText.str8 = newText.str8;
+    currentText.str9 = newText.str9;
+    currentText.str10 = newText.str10;
+    currentText.str11 = newText.str11;
+    currentText.str12 = newText.str12;
+    currentText.str13 = newText.str13;
+    currentText.str14 = newText.str14;
+    currentText.str15 = newText.str15;
+    currentText.str16 = newText.str16;
 
     return currentText;
 }
 
-export function replaceEntity(current: Text | Card, _new: Text | YGOProCard) {
-    if ("name" in current && "name" in _new) {
+export function replaceEntity(current: Text | Card, _new: any) {
+    if (current instanceof Text && _new instanceof Text) {
         return replaceText(current, _new);
-    } else if ("atk" in current && "atk" in _new) {
+    } else if (current instanceof Card && _new instanceof YGOProCard) {
         return replaceCard(current, _new);
     }
 }
